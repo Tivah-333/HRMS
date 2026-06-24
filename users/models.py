5303571922899b75c1b58c11707a15a3935dcb41
Tivah-333 <ainebyoonadativah@gmail.com>
2026-06-24 11:12:15 +0300

    Initial HRMS setup with users app

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = [
+        ('hr_officer', 'HR Officer'),
+        ('hr_director', 'HR Director'),
+        ('head', 'Department Head'),
+        ('management', 'Management'),
+        ('board', 'Board Member'),
+        ('employee', 'Employee'),
+        ('applicant', 'Job Applicant'),
+        ('trainee', 'Graduate Trainee'),
+        ('intern', 'Student Intern'),
+    ]
+
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
